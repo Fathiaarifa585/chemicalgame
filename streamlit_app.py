beb111b9c96e8d25e75bcecd2c9aca0c6446dab7
Fathiaarifa585 <fathiamnur585@gmail.com>
2025-07-14 19:10:59 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,8 @@
 import streamlit as st
 st.title("GAME UJI KUALITATIF SENYAWA")
 st.write("SELAMAT DATANG DI GAME EDUKASI")
+
+#MENAMPILKAN MENU UTAMA#
+def menampilkan_menu_utama():
+  print("ORGANIK")
+  print("LANORGANIK")
