15106a4667ab79358c0cf05eb8b46226f2308cbd
Fathiaarifa585 <fathiamnur585@gmail.com>
2025-07-14 20:01:24 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,11 +3,6 @@ import streamlit as st
 st.title("Halo Selamat Datang Di Game Kami!")
 st.title("Mau Ngapain Dulu Nih?")
 
-import time
-import numpy as np
-import pandas as pd
-import streamlit as st
-
 _LOREM_IPSUM = """
 Di materi ini kamu akan mempelajari onorganik. ada latoihan dan materinya juga. kamu mau pilih yang mana dulu?
 """
@@ -15,11 +10,13 @@ Di materi ini kamu akan mempelajari onorganik. ada latoihan dan materinya juga. 
 
 def stream_data():
     for word in _LOREM_IPSUM.split(" "):
-
-    yield pd.DataFrame(
-        np.random.randn(5, 10),
-        columns=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"],
-    )
+        left, middle, right = st.columns(3)
+        if left.button("Plain button", use_container_width=True):
+            left.markdown("You clicked the plain button.")
+        if middle.button("Emoji button", icon="😃", use_container_width=True):
+            middle.markdown("You clicked the emoji button.")
+        if right.button("Material button", icon=":material/mood:", use_container_width=True):
+            right.markdown("You clicked the Material button.")
 
 if st.button("Stream data"):
     st.write_stream(stream_data)
