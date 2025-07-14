1a6f5c17f5d17651689f68b10a8d650ec93e7be5
Fathiaarifa585 <fathiamnur585@gmail.com>
2025-07-14 19:48:49 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,27 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
-_KIMIA_ORGANIK = """Di materi ini kamu akan mempelajari materi terkait kimia anorganik, meliputi Uji nyala dan Uji Kualitatif senyawa anorganik"""
+_LOREM_IPSUM = """
+Lorem ipsum dolor sit amet, **consectetur adipiscing** elit, sed do eiusmod tempor
+incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+"""
 
-if st.button("KIMIA ORGANIK"):
-    st.write_stream(_KIMIA_ORGANIK)
+
+def stream_data():
+    for word in _LOREM_IPSUM.split(" "):
+        yield word + " "
+        time.sleep(0.02)
+
+    yield pd.DataFrame(
+        np.random.randn(5, 10),
+        columns=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"],
+    )
+
+    for word in _LOREM_IPSUM.split(" "):
+        yield word + " "
+        time.sleep(0.02)
+
+
+if st.button("Stream data"):
+    st.write_stream(stream_data)
