ac0bc9cb701379b3926b89a18273eea9d27ecf4b
Fathiaarifa585 <fathiamnur585@gmail.com>
2025-07-14 19:43:59 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,16 @@
 import streamlit as st
 
 st.title("Halo Selamat Datang Di Game Kami!")
-st.title("Mau Ngapain Dulu NIh?")
+st.title("Mau Ngapain Dulu Nih?")
+
+import time
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+_KIMIA_ORGANIK = """
+HALO!!!Di materi ini kamu akan mempelajari materi terkait kimia anorganik, meliputi Uji nyala dan Uji Kualitatif senyawa anorganik
+"""
+
+if st.button("KIMIA ORGANIK"):
+    st.write_stream(KIMIA_ORGANIK)
