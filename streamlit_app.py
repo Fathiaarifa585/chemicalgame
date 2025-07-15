b3bf641224be52fbdeb08cc56d269a77e27a31e4
Fathiaarifa585 <fathiamnur585@gmail.com>
2025-07-15 10:28:35 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,14 +11,15 @@ Di materi ini kamu akan mempelajari onorganik. ada latoihan dan materinya juga. 
 def stream_data():
     for word in _LOREM_IPSUM.split(" "):
         print word in _LOREM_IPSUM.split(" ")
-        import streamlit as st
-        left, middle, right = st.columns(3)
-        if left.button("Plain button", use_container_width=True):
-            left.markdown("You clicked the plain button.")
-        if middle.button("Emoji button", icon="😃", use_container_width=True):
-            middle.markdown("You clicked the emoji button.")
-        if right.button("Material button", icon=":material/mood:", use_container_width=True):
-            right.markdown("You clicked the Material button.")
+        
+import streamlit as st
+left, middle, right = st.columns(3)
+if left.button("Plain button", use_container_width=True):
+    left.markdown("You clicked the plain button.")
+if middle.button("Emoji button", icon="😃", use_container_width=True):
+     middle.markdown("You clicked the emoji button.")
+if right.button("Material button", icon=":material/mood:", use_container_width=True):
+    right.markdown("You clicked the Material button.")
 
 if st.button("Stream data"):
     st.write_stream(stream_data)
